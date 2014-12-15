61dd73b3191293818a42dd5a27549259aae36417
Christopher Wan <christopher.wan@yale.edu>
2014-12-14 22:38:58 -0500

    begin removing static stuff

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,18 @@ var mongoose = require('mongoose');
 var Obj = mongoose.model('Obj');
 
 /* GET home page. */
-router.get('/', function(req, res) {
-    res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+// 	Obj.find().toArray(function (error, objs) {
+// 		res.render('index', { 
+// 			title: 'Express',
+// 			objs: objs || [] 
+// 		});
+// 	});
+// });
 
+router.get('/', function(req, res) {
+	res.render('index', { title: 'Add New User' });
+});
 
 
 /* GET Userlist page. */
@@ -54,22 +62,6 @@ router.post('/add', function(req, res) {
     	}
     });
     
-    // // Submit to the DB
-    // collection.insert({
-    //     "nameObj" : nameObj,
-    //     "locationObj" : locationObj,
-    //     "user" : user
-    // }, function (err, doc) {
-    //     if (err) {
-    //         // If it failed, return error
-    //         res.send("There was a problem adding the information to the database.");
-    //     }
-    //     else {
-    //         // If it worked, set the header so the address bar doesn't still say /adduser
-    //         // And forward to success page
-    //         res.send("Success");
-    //     }
-    // });
 });
 
 module.exports = router;
